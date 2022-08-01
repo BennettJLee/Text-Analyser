09e325aed7aa57499cf480863eb37053c7d7e08e
BennettJLee <benjlee9@gmail.com>
2022-08-02 00:00:33 +1200

    first commit

---
+++ Text Analysis/Text Analysis/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Text_Analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxTotalLines = new System.Windows.Forms.TextBox();
+            this.textboxTotalWords = new System.Windows.Forms.TextBox();
+            this.textBoxEnterWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSearchWord = new System.Windows.Forms.Button();
+            this.textBoxWordAppears = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxRichness = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openTextToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openTextToolStripMenuItem
+            // 
+            this.openTextToolStripMenuItem.Name = "openTextToolStripMenuItem";
+            this.openTextToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openTextToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.openTextToolStripMenuItem.Text = "Open File";
+            this.openTextToolStripMenuItem.Click += new System.EventHandler(this.openTextToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxTotalLines
+            // 
+            this.textBoxTotalLines.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTotalLines.Location = new System.Drawing.Point(626, 53);
+            this.textBoxTotalLines.Name = "textBoxTotalLines";
+            this.textBoxTotalLines.ReadOnly = true;
+            this.textBoxTotalLines.Size = new System.Drawing.Size(107, 31);
+            this.textBoxTotalLines.TabIndex = 1;
+            // 
+            // textboxTotalWords
+            // 
+            this.textboxTotalWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxTotalWords.Location = new System.Drawing.Point(626, 121);
+            this.textboxTotalWords.Name = "textboxTotalWords";
+            this.textboxTotalWords.ReadOnly = true;
+            this.textboxTotalWords.Size = new System.Drawing.Size(107, 31);
+            this.textboxTotalWords.TabIndex = 2;
+            // 
+            // textBoxEnterWord
+            // 
+            this.textBoxEnterWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEnterWord.Location = new System.Drawing.Point(626, 181);
+            this.textBoxEnterWord.Name = "textBoxEnterWord";
+            this.textBoxEnterWord.Size = new System.Drawing.Size(148, 31);
+            this.textBoxEnterWord.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(623, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total Lines:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(623, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total Words:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(623, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter your word:";
+            // 
+            // buttonSearchWord
+            // 
+            this.buttonSearchWord.Location = new System.Drawing.Point(626, 219);
+            this.buttonSearchWord.Name = "buttonSearchWord";
+            this.buttonSearchWord.Size = new System.Drawing.Size(55, 23);
+            this.buttonSearchWord.TabIndex = 9;
+            this.buttonSearchWord.Text = "Search";
+            this.buttonSearchWord.UseVisualStyleBackColor = true;
+            this.buttonSearchWord.Click += new System.EventHandler(this.buttonSearchWord_Click);
+            // 
+            // textBoxWordAppears
+            // 
+            this.textBoxWordAppears.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxWordAppears.Location = new System.Drawing.Point(626, 275);
+            this.textBoxWordAppears.Name = "textBoxWordAppears";
+            this.textBoxWordAppears.ReadOnly = true;
+            this.textBoxWordAppears.Size = new System.Drawing.Size(148, 31);
+            this.textBoxWordAppears.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(623, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Word Appears:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(591, 406);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(623, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Distinct Words:";
+            // 
+            // textBoxRichness
+            // 
+            this.textBoxRichness.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRichness.Location = new System.Drawing.Point(626, 339);
+            this.textBoxRichness.Name = "textBoxRichness";
+            this.textBoxRichness.ReadOnly = true;
+            this.textBoxRichness.Size = new System.Drawing.Size(107, 31);
+            this.textBoxRichness.TabIndex = 13;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(626, 376);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(66, 23);
+            this.buttonCalculate.TabIndex = 14;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxRichness);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxWordAppears);
+            this.Controls.Add(this.buttonSearchWord);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEnterWord);
+            this.Controls.Add(this.textboxTotalWords);
+            this.Controls.Add(this.textBoxTotalLines);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxTotalLines;
+        private System.Windows.Forms.TextBox textboxTotalWords;
+        private System.Windows.Forms.TextBox textBoxEnterWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSearchWord;
+        private System.Windows.Forms.TextBox textBoxWordAppears;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxRichness;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ Text Analysis/Text Analysis/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Text_Analysis
+{
+    public partial class Form1 : Form
+    {
+
+        //Name: Ben Lee
+        //Id: 1567783
+
+        //For some reason the richTextBox doesnt load big text files, so some of the text files
+        //won't load, so I tested with bacon1.txt
+
+        List<string> WordsList = new List<string>();
+        List<string> distinctList = new List<string>();
+
+        const string FILTER = "TXT FILES|*.txt|All Files|*.*";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// initialises the word list
+        /// </summary>
+        private void Initialise()
+        {
+            WordsList.Clear();
+        }
+        /// <summary>
+        /// This opens the text and adds elements to the wordsList
+        /// and displays the total words and lines
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            string[] csvArray;
+            StreamReader reader;
+            int lineCount = 0;
+            int totalWords = 0;
+            char[] delimiterChars = {' ', ',', '.', ':', '\t'};
+
+            //Set the filter for the dialog control
+            openFileDialog1.Filter = FILTER;
+            //Check if the user selected a file to open
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //open the selected file
+                reader = File.OpenText(openFileDialog1.FileName);
+
+                Initialise();
+                //repeat while it is not the end of file
+                while (!reader.EndOfStream)
+                {
+                    //read a line from the file
+                    line = reader.ReadLine();
+                    richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+
+                    //Split the values in the line using an array
+                    csvArray = line.Split(delimiterChars);
+                    csvArray = line.Split(null);
+                    try
+                    {
+                        lineCount++;
+                        totalWords += csvArray.Length;
+                        
+                        for(int i = 0; i < csvArray.Length; i++)
+                        {
+                            WordsList.Add(csvArray[i].ToLower());
+                            
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error: " + line);
+                    }
+                }
+
+                textBoxTotalLines.Text =  lineCount.ToString();
+                textboxTotalWords.Text = totalWords.ToString();
+                reader.Close();
+                this.Text = openFileDialog1.FileName;
+            }
+        }
+        /// <summary>
+        /// This searches though the list and checks if they are the same as the search word
+        /// if they are they will conuted and displayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSearchWord_Click(object sender, EventArgs e)
+        {
+            string searchWord = textBoxEnterWord.Text.ToLower();
+            int index = WordsList.IndexOf(searchWord);
+            int WordCount = 0;
+            decimal WordPercentage = 0;
+            //decimal TotalWords = WordsList.Count;
+            decimal TotalWords = WordsList.Count;
+            //For every element 
+            for (int i = 0; i < WordsList.Count; i++)
+            {
+                //Add to total if the element equals the search word
+                if (WordsList[i] == searchWord)
+                {
+                    WordCount++;
+                }
+            }
+            textBoxWordAppears.Text = WordCount.ToString() + " times";
+            WordPercentage = WordCount / TotalWords;
+            MessageBox.Show("This word shows up " + WordPercentage.ToString("P") + " of the time");
+        }
+        /// <summary>
+        /// This calculates the total distinct words and the richness of the text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            decimal totalWords = WordsList.Count;
+            decimal distinctWords = distinctList.Count;
+            decimal richness = 0;
+
+            //Insert distinct elements from wordsList to distinctList 
+            distinctList = WordsList.Distinct().ToList();
+            //Calculate the richness of the text
+            richness = distinctList.Count / totalWords;
+
+            textBoxRichness.Text = distinctList.Count().ToString();
+            MessageBox.Show("This text has a richness of " + richness.ToString("P"));
+        }
+    }
+}
